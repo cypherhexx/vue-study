760227768a774b9c15218d77ade85dec87ea5a37
codeproline <webdev410219@gmail.com>
2021-02-07 21:25:05 +0800

    test commit

--- src/components/common/backToTop.vue
+++ src/components/common/backToTop.vue
@@ -14,6 +14,7 @@
         },
         computed: {
             theme() {
+                conosle.log("test commit")
                 return this.$store.getters.THEME_COLOR
             },
         },
